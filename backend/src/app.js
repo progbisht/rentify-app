58805d4868290d017a3c646304eb8620591006e6
Kailash <bishtkailash98@gmail.com>
2024-05-28 04:59:10 +0530

    Updated file structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')))
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')))
 
 
 app.use('/public/uploads', express.static(path.join(__dirname, 'public', 'uploads')))
@@ -23,6 +23,26 @@ app.use('/public/uploads', express.static(path.join(__dirname, 'public', 'upload
 app.use('/api/v1/users', require('./routers/userRoutes'));
 app.use('/api/v1/places', require('./routers/placeRoutes'));
 app.use('/api/v1/wishlist', require('./routers/wishlistRoutes'));
+
+
+// Frontend routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html' ))
+})
+
+app.all('*', (req,res) => {
+    res.status(404)
+
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404Error.html'))
+    }
+    else if(req.accepts('json')){
+        res.json({ message : "Resource not found" })
+    }
+    else{
+        res.txt("Resource not found")
+    }
+})
 
 
 
